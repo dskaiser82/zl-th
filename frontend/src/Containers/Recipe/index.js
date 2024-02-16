af6f9fc2fdeb60fc4f7b1455af0f958a44cd3949
Dan Kaiser <dskaiser82@gmail.com>
2024-02-15 20:26:54 -0800

    Add todos

--- frontend/src/Containers/Recipe/index.js
+++ frontend/src/Containers/Recipe/index.js
@@ -1,4 +1,20 @@
 import React, { useState, useEffect } from "react"
+/* 
+1. Add recipe API Call
+
+### /api/recipe/:id
+\
+
+Front end
+1. Add RECIPE Details page for Recipe API cal
+1. add a recipe component to display the response (lnk to page)
+1.FIX REDUX Actions
+1. fix the search function - DONE
+1. add routing so that you can land on a recipe page or reload and have the same search - DONE
+
+
+*/
+
 const Recipe = () => {
   return <div>Im a recipe</div>
 }
