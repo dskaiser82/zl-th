d1850991bba2d2a48ff51613d79b112d045483b9
Dan Kaiser <dskaiser82@gmail.com>
2024-02-19 11:57:14 -0800

    style recipe page

--- frontend/src/Containers/Recipe/index.tsx
+++ frontend/src/Containers/Recipe/index.tsx
@@ -1,32 +1,59 @@
 import React, { useState, useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 import { AnyAction } from "redux"
 import { ThunkDispatch } from "redux-thunk"
 import { AppState } from "../../reducers"
 import { HomeWrapper } from "../Home/styles"
 import * as actions from "../../actions"
-import { buttonStyles, StyledDiv } from "./styles"
+import { StyledInstructions, StyledFlexHero } from "./styles"
+import { LinearProgress } from "@material-ui/core"
 
 const Recipe = () => {
   const dispatch: ThunkDispatch<AppState, null, AnyAction> = useDispatch()
+  const { recipeDetails, isLoading } = useSelector(
+    (state: AppState) => state.recipe
+  )
   const { id } = useParams()
 
   useEffect(() => {
     dispatch(actions.dispatchGetRecipe(id))
   }, [])
 
-  // fetch(`http://localhost:4000/api/recipe/${id}`, {
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // })
-  //   .then((response) => response.json())
-  //   .then((res) => console.log(res))
-
   return (
     <HomeWrapper>
-      <StyledDiv className={buttonStyles}>Im a recipe</StyledDiv>
+      <a
+        href="/"
+        style={{ fontSize: "50px", textDecoration: "none", color: "black" }}
+      >
+        {"\u2190"}
+      </a>
+      {recipeDetails?.name && (
+        <div>
+          <h1> {recipeDetails?.name}</h1>
+          <StyledFlexHero>
+            <img
+              style={{ width: "300px" }}
+              src="https://theme-assets.getbento.com/sensei/74ec6d7.sensei/assets/images/catering-item-placeholder-704x520.png"
+              alt="placeholder recipe food"
+            />
+
+            {recipeDetails?.ingredients.map((ing) => {
+              return (
+                <ul>
+                  <li>
+                    {ing.unit} of {ing.name} / {ing.amount} units
+                  </li>
+                </ul>
+              )
+            })}
+          </StyledFlexHero>
+
+          <StyledInstructions>{recipeDetails.instructions}</StyledInstructions>
+        </div>
+      )}
+
+      {isLoading && <LinearProgress />}
     </HomeWrapper>
   )
 }
--- frontend/src/Containers/Recipe/styles.ts
+++ frontend/src/Containers/Recipe/styles.ts
@@ -1,17 +1,21 @@
 import styled, { css } from "styled-components"
 
-export const buttonStyles = css({
-  backgroundColor: "blue",
-  color: "white",
-  padding: "10px 20px",
-  border: "none",
-  borderRadius: "5px",
-  cursor: "pointer",
-  "&:hover": {
-    backgroundColor: "darkblue",
-  },
+export const BodyInstructionCSS = css({
+  fontSize: "16px",
+  margin: "12px",
 })
 
-export const StyledDiv = styled.div`
-  ${buttonStyles}
+export const FlexHeroCSS = css({
+  display: "flex",
+  flexDirection: "column",
+  gap: "5px",
+  alignItems: "start",
+})
+
+export const StyledInstructions = styled.div`
+  ${BodyInstructionCSS}
+`
+
+export const StyledFlexHero = styled.div`
+  ${FlexHeroCSS}
 `
