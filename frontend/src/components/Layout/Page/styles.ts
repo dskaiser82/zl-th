a532946734045fd56c5d9218445b4296f80521e7
Dan Kaiser <dskaiser82@gmail.com>
2024-02-25 13:21:40 -0800

    remove vh

--- frontend/src/components/Layout/Page/styles.ts
+++ frontend/src/components/Layout/Page/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components"
 import { baseLayoutStyles } from "../shared"
 
 export const Main = styled.main`
-  height: 90vh;
   display: flex;
   flex-flow: column;
   background-color: ${(props) => props.theme.background};
